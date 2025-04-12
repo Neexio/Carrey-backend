98e11b0293dbd7d4d02a86268a1d6a855687107a
Neexio <Eiriklarsen2010@gmail.com>
2025-04-13 00:09:21 +0200

    Remove oversized ZIP file

--- backup/2025-04-12/carrey-seo-dashboard/config/wp-config.php
+++
@@ -1,7 +0,0 @@
-// ** Database settings - You can get this info from your web host ** //
-define('DB_NAME', 'carrfacy_wpq8');
-define('DB_USER', 'carrfacy_wpq8');
-define('DB_PASSWORD', 'Eirik16498991');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', ''); 
--- backup_restore/www/wp-content/plugins/animation-addons-for-elementor/inc/admin/dashboard/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '0912adce207d2c15dfff');
