42a4210cb7aa37c964d731f50a8ba6a8a0a0c3fb
Neexio <Eiriklarsen2010@gmail.com>
2025-04-15 04:04:25 +0200

    Oppdatert child theme CSS og functions.php for bedre stilhåndtering

--- public_html/wp-content/themes/sassly-child/functions.php
+++ public_html/wp-content/themes/sassly-child/functions.php
@@ -2,13 +2,21 @@
 /**
  * Theme functions and definitions.
  */
+// Properly enqueue parent and child theme styles
 function sassly_child_enqueue_styles() {
-    
-    wp_enqueue_style( 'sassly-child-style',
+    // Parent theme style
+    wp_enqueue_style(
+        'sassly-parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    // Child theme style
+    wp_enqueue_style(
+        'sassly-child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        [],
+        array('sassly-parent-style'),
         wp_get_theme()->get('Version')
     );
 }
 
-add_action(  'wp_enqueue_scripts', 'sassly_child_enqueue_styles', 16);
+add_action('wp_enqueue_scripts', 'sassly_child_enqueue_styles');
